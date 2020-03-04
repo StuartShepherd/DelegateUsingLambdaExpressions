1cf4b847d683e2dc88cba2c97ffeef07844150e1
stuartjshepherd <stuart@createstudios.com.au>
2020-03-05 08:51:44 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegateUsingLambdaExpressions
+{
+    class Program
+    {
+        public delegate double MathDelegate(double value1, double value2);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate using Lambda Expressions");
+            Console.WriteLine("Sometimes the whole signature of a method can be more code than the body of a method. There are also situations in which you need to create an entire method only to use it in a delegate.");
+            Console.WriteLine();
+
+            // output result is 7   
+            MathDelegate mathAddDelegate = (x, y) => x + y;
+            var result = mathAddDelegate(5, 2);
+            Console.WriteLine(result);
+
+            // output result is 3
+            MathDelegate mathSubtractDelegate = (x, y) => x - y; ;
+            result = mathSubtractDelegate(5, 2);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Delegate using Lambda Expressions
+Sometimes the whole signature of a method can be more code than the body of a method. There are also situations in which you need to create an entire method only to use it in a delegate.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
